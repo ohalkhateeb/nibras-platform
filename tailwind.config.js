ff5916a6586574449f3442a3e6800bd1288acc17
ohalkhateeb <ohalkhateeb@gmail.com>
2025-09-25 13:11:24 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 // tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- This line is essential
   ],
   theme: {
     extend: {},
